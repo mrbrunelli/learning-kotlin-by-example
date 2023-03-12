7f01b022e29111ec0d1e17dc075b1bb24e779b8e
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-12 10:09:15 -0300

    add multipy unit test

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,9 +6,12 @@ class MainKtTest {
     @Test
     fun sum() {
         assertEquals(4, sum(2, 2))
+        assertEquals(12, sum(8, 4))
     }
 
     @Test
     fun multiply() {
+        assertEquals(10, multiply(5, 2))
+        assertEquals(24, multiply(3,8))
     }
 }
