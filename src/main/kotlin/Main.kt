8a5360fd3a4c7fc0c17b4e09386b64bc0eb7cde7
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-12 10:03:16 -0300

    add junit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun printMessage(message: String): Unit {
+    println(message)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun printMessageWithPrefix(message: String, prefix: String = "Info"): Unit {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+fun main() {
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(1, 2))
+    println(multiply(2, 4))
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -3,17 +3,9 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 class MainKtTest {
-
-    @Test
-    fun printMessage() {
-    }
-
-    @Test
-    fun printMessageWithPrefix() {
-    }
-
     @Test
     fun sum() {
+        assertEquals(4, sum(2, 2))
     }
 
     @Test
