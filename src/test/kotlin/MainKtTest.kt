5cf6d593e5c63e672b167d82d025f2f6ca38ce1d
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-12 10:02:54 -0300

    start new kotlin app

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @Test
+    fun printMessage() {
+    }
+
+    @Test
+    fun printMessageWithPrefix() {
+    }
+
+    @Test
+    fun sum() {
+    }
+
+    @Test
+    fun multiply() {
+    }
+}
