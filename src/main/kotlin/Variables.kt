9551338c131e669425120f281dbb791cd98f51ad
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-13 07:14:59 -0300

    add variables example

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var a: String = "initial"
+    println(a)
+
+    val b: Int = 1
+    val c = 3
+    println(b)
+    println(c)
+
+    var e: Int
+    // println(e) compiler error
+    e = 50
+    println(e)
+}
