6581240ea64178532341b6e8c3acd9563315e13c
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-14 08:50:16 -0300

    add loop examples

---
+++ src/main/kotlin/For.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {
+        println("Yummy, it's a $cake cake!")
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/kotlin/While.kt
@@ -0,0 +1,17 @@
+fun eatACake() = println("Eat a Cake")
+fun bakeACake() = println("Bake a Cake")
+
+fun main() {
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    while (cakesEaten < 3) {
+        eatACake()
+        cakesEaten++
+    }
+
+    do {
+        bakeACake()
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+}
