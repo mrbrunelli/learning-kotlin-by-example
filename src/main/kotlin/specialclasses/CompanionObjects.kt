7220b3442e2418629339070bac29fafc99291840
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-23 08:42:50 -0300

    add object declaration and companion objects examples

---
+++ src/main/kotlin/specialclasses/CompanionObjects.kt
@@ -0,0 +1,15 @@
+package specialclasses
+
+class BigBen {
+    companion object Bonger {
+        fun getBongs(nTimes: Int) {
+            for (i in 1..nTimes) {
+                print("BONG ")
+            }
+        }
+    }
+}
+
+fun main() {
+    BigBen.getBongs(12)
+}
