6cbfa851a6bb8eb3616cc71e1ec638fbf8439381
mrbrunelli <matheus.brunelli@gmail.com>
2023-03-22 22:42:14 -0300

    add object keyword example

---
+++ src/main/kotlin/SpecialClasses/ObjectKeyword.kt
@@ -0,0 +1,19 @@
+package SpecialClasses
+
+import java.util.Random
+
+
+class LuckDispatcher {
+    fun getNumber() {
+        var objRandom = Random()
+        println(objRandom.nextInt(90))
+    }
+}
+
+fun main() {
+    val d1 = LuckDispatcher()
+    val d2 = LuckDispatcher()
+
+    d1.getNumber()
+    d2.getNumber()
+}
